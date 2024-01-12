fed93824ebf64fe15ed377c238b69b7f3cc3a9db
Ida Whitcomb <idawhitcomb@gmail.com>
2024-01-12 12:12:34 -0500

    Filled in basic information to begin project.

---
+++ models/Blog.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {
+//  checkPassword(loginPw) {
+//    return bcrypt.compareSync(loginPw, this.password);
+//  }
+} 
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date: {
+        type: DataTypes.DECIMAL,
+        allowNull: false
+    }
+  },
+  {
+//    hooks: {
+//      beforeCreate: async (newUserData) => {
+//        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+//        return newUserData;
+//      },
+//    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blog',
+  }
+);
+
+module.exports = Blog;

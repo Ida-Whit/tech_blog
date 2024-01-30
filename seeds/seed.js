dad1fd284ee29e082f4982214c2fce312c70cf21
Ida Whitcomb <idawhitcomb@gmail.com>
2024-01-30 13:40:45 -0500

    Began formatting handlebars pages

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,14 +1,24 @@
 const sequelize = require("../config/connection");
-const { Blog } = require("../models");
+const { User, Blog, Comment } = require("../models");
 const blogData = require("../seeds/blogData.json");
+const userData = require("./userData.json");
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
     try {
         await sequelize.sync({ force: true });
-        const blog = await Blog.bulkCreate(blogData, {
+        const users = await User.bulkCreate(userData, {
             individualHooks: true,
             returning: true,
         });
+        const blogs = await Blog.bulkCreate(blogData, {
+            individualHooks: true,
+            returning: true,
+        });
+        const comments = await Comment.bulkCreate(commentData, {
+            individualHooks: true,
+            returning: true,
+        })
         console.log("Database seeded!");
     } catch (error) {
         console.error("Error seeding database", error);

47990f2b8fc04d389a8017de93e0b396474d4018
Ida Whitcomb <idawhitcomb@gmail.com>
2024-02-08 15:26:50 -0500

    Filled in javascript for commenting (comment.js)

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,4 +1,28 @@
 const addComment = async (event) => {
     event.preventDefault();
-    const comment = document.querySelector('').value.trim();
-}
+    const comment = document.querySelector('#comment').value.trim();
+    let blog_id = event.target.dataset.blog_id
+    blog_id = perseInt(blog_id)
+    if (comment) {
+        try {
+            const response = await fetch ('/api/comment', {
+                method: 'POST',
+                body: JSON.stringify({ comment, blog_id }),
+                header: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (response.ok) {
+                document.location.reload();
+            } else {
+                alert('Failed to add comment.');
+            }
+        } catch (error) {
+            console.error('Error adding comment:', error);
+        }
+    }
+};
+
+document
+  .querySelector('#commentBtn')
+  .addEventListener('click', addComment);
